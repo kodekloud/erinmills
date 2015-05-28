80147c48737158e35506cb5979f7c1d8f419d696
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-05-28 11:41:57 -0400

    via kodekloud.io (554a79236e6f64713f050000)

--- assets/application.js
+++ assets/application.js
@@ -1,9 +1,7 @@
 /*Created 2015-05-18  by RKS*/
 function init(){
-    <script>
-            $(document).ready(function() {
                 $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
-            });
+
             
             $(".footer_feature_box").hover(function(){
                 switch (this.id){
@@ -37,4 +35,6 @@ function init(){
                     ;
                     
                 }
+            });
+
 }
