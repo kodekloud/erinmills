c0bb87597a470b8800db1bf05430b66d2169ad66
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-06-08 11:51:16 -0400

    via kodekloud.io (554a79236e6f64713f050000)

--- assets/application.js
+++ assets/application.js
@@ -44,17 +44,17 @@ function renderLayoutHours(container, template, collection){
     item_list.push(collection);
         $.each( item_list , function( key, val ) {
             if (val.open_time && val.close_time && val.is_closed == false){
-                var open_time = new Date (val.open_time)
-                var close_time = new Date (val.close_time)
+                var open_time = new Date (val.open_time);
+                var close_time = new Date (val.close_time);
                 val.open_time = convert_hour(open_time);
                 val.close_time = convert_hour(close_time);    
                 val.h = val.open_time+ " - " + val.close_time;
             } else {
-                "Closed"
+                "Closed";
             }
             
             var rendered = Mustache.render(template_html,val);
-            console.log(val)
+            console.log(val);
             item_rendered.push(rendered);
         });
         $(container).html(item_rendered.join(''));
