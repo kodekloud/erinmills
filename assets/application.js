18dedef1efb7d9cbd50dfad29f3bb1abd52aa0d7
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-06 16:27:07 -0400

    via kodekloud.io (554a79236e6f64713f050000)

--- assets/application.js
+++ assets/application.js
@@ -4,7 +4,7 @@ function init(){
     var collapse_shopping = ["/hours", "/map", "/stores", "/pages/erinmills-location"];
     var collapse_events = ["/events", "/promotions", "/redevlopment", "/trend-report", "/pages/erinmills-video-gallery"];
     var collapse_guest_services = ["/pages/erinmills-gift-cards", "/pages/erinmills-accessibility", "/pages/erinmills-guest-services", "/pages/erinmills-security-services"];
-    var collapse_centre_info = [];
+    var collapse_centre_info = ["/pages/erinmills"];
     
     
 }
