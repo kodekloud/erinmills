ee069ac4df4e9f93049583a9f26a767bfeeece46
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-26 18:14:53 -0400

    via kodekloud.io (554a79236e6f64713f050000)

--- assets/application.js
+++ assets/application.js
@@ -80,7 +80,7 @@ function renderSideEvents(container, template, collection, type){
                 all_promos.push(val);
             } 
         });
-        $.each( all_promos , function( key, val ) {
+        $.each( item_list , function( key, val ) {
             if (val.name.length > 60 ) {
                val.name_shortened =  val.name.substring(0,60)+'...';
             } 
